cea8bc1a41d618a23a1c18f80a5c0bb4d57bdeac
Jeanre Swanepoel <jeanre.swanepoel@gmail.com>
2021-06-25 16:30:51 +0200

    vim configuration, tmux configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s ~/.dots/configs/vimrc ~/.vimrc
 ln -s ~/.dots/configs/zshrc ~/.zshrc
+ln -s ~/.dots/configs/tmux.conf ~/.tmux.conf
