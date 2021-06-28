82a249f0a6274b1f27576575dd54a37ecac28801
Jeanre Swanepoel <jeanre.swanepoel@gmail.com>
2021-06-28 15:51:12 +0200

    vim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.dots/configs/vimrc ~/.vimrc
 ln -s ~/.dots/configs/zshrc ~/.zshrc
